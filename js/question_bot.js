33a280db76b37d5aa561cd2785856dda29a46490
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2019-02-02 09:28:06 +0100

    add horizontal align for gifs

--- js/question_bot.js
+++ js/question_bot.js
@@ -349,7 +349,7 @@ function css_rgba_random_color() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    const a = 0.75;
+    const a = 0.4;
     return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'
 }
 
